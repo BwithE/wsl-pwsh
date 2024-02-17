a3129d34a0ebfcbb110ea3f62a8a0ec195214c9f
BwithE <144924113+BwithE@users.noreply.github.com>
2024-02-16 22:13:11 -0500

    Update wsl-install.ps1

--- wsl-install.ps1
+++ wsl-install.ps1
@@ -3,26 +3,24 @@ Clear-host
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     clear-host
     Write-Host "Please run this script as an administrator." -foregroundcolor Yellow
-    exit
-}
-else{
-# Check if WSL is already enabled
-$wslStatus = Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-
-if ($wslStatus.State -eq "Enabled") {
-    Write-Host "WSL is already enabled." -foregroundcolor Green
 } else {
-    # Enable WSL
-    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+    # Check if WSL is already enabled
+    $wslStatus = Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 
-    # Check if reboot is required
-    $rebootRequired = $?
-    if ($rebootRequired) {
-        Write-Host "WSL has been enabled. Please reboot your computer to apply the changes."
+    if ($wslStatus.State -eq "Enabled") {
+        Write-Host "WSL is already enabled." -foregroundcolor Green
     } else {
-        Write-Host "WSL has been enabled successfully."
+        # Enable WSL
+        Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+
+        # Check if reboot is required
+        $rebootRequired = $?
+        if ($rebootRequired) {
+            Write-Host "WSL has been enabled. Please reboot your computer to apply the changes."
+        } else {
+            Write-Host "WSL has been enabled successfully."
+        }
     }
-}
 
 # Turn on WSL
 #wsl --set-default-version 2
